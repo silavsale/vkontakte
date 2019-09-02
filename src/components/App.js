eeb18e93d173b48fa9fb113bc5ca20f6746800a5
Sergiy Rudenko <silavsale@gmail.com>
2019-09-01 20:31:33 -0400

    clean project

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
-import UserContainer from '../containers/UserContainer' // изменили импорт
-import PageContainer from '../containers/PageContainer' // изменили импорт
 
 class App extends Component {
   render() {
-    return (
-      <div className="app">
-        <PageContainer />
-        <UserContainer />
-      </div>
-    )
+    return <div className="app"></div>
   }
 }
 
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { pageReducer } from './page'
-import { userReducer } from './user'
-
-export const rootReducer = combineReducers({
-  page: pageReducer,
-  user: userReducer,
-})
